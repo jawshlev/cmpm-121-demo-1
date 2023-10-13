99aa291493bf471c83269cc0582e00509fc8a5a2
Tvilchis <soybr3ad@gmail.com>
2023-10-13 01:19:46 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ const gameName = "Josh's game";
 
 document.title = gameName;
 
+const myButton = document.getElementById("counterButton")!;
+let count: number = 0;
+const countDisplay = document.getElementById("countDisplay")!;
+
+myButton.addEventListener("click", () => {
+  count++;
+  countDisplay.textContent = `Count: ${count}🍣`;
+});
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);

cfa3636be8a3027e61979b65e497a7e4e9a303d7
Tvilchis <soybr3ad@gmail.com>
2023-10-13 01:36:54 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,18 @@ document.title = gameName;
 
 const myButton = document.getElementById("counterButton")!;
 let count: number = 0;
-const countDisplay = document.getElementById("countDisplay")!;
 
-myButton.addEventListener("click", () => {
-  count++;
+function updateCountDisplay() {
+  const countDisplay = document.getElementById("countDisplay")!;
   countDisplay.textContent = `Count: ${count}🍣`;
-});
+}
+function incrementCounter() {
+  count++;
+  updateCountDisplay();
+}
+setInterval(incrementCounter, 1000);
+myButton.addEventListener("click", incrementCounter);
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);

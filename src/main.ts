3649a6e587c1e047a40471342298a8c5653957c5
Tvilchis <soybr3ad@gmail.com>
2023-10-13 01:57:56 -0700

    Step 4 Complete

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,32 @@ const gameName = "Josh's game";
 
 document.title = gameName;
 
-const myButton = document.getElementById("counterButton")!;
 let count: number = 0;
+let lastFrameTime = performance.now();
 
 function updateCountDisplay() {
   const countDisplay = document.getElementById("countDisplay")!;
-  countDisplay.textContent = `Count: ${count}🍣`;
+  countDisplay.textContent = `Count: ${Math.round(count)}🍣`;
 }
+
 function incrementCounter() {
   count++;
   updateCountDisplay();
 }
-setInterval(incrementCounter, 1000);
+
+function animateCounter(timestamp: number) {
+  const elapsedSeconds = (timestamp - lastFrameTime) / 1000;
+  count += (1 / 60) * elapsedSeconds;
+
+  incrementCounter();
+
+  lastFrameTime = timestamp;
+  requestAnimationFrame(animateCounter);
+}
+
+requestAnimationFrame(animateCounter);
+
+const myButton = document.getElementById("counterButton")!;
 myButton.addEventListener("click", incrementCounter);
 
 const header = document.createElement("h1");

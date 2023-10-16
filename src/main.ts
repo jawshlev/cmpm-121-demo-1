c7c1a926c07b10750dce4dca62e96c86bea82857
Tvilchis <soybr3ad@gmail.com>
2023-10-15 23:04:03 -0700

    Step 10 complete

--- src/main.ts
+++ src/main.ts
@@ -23,10 +23,15 @@ const growthRateDisplay = document.getElementById("growthRateDisplay")!;
 const upgradeCountSafeway = document.getElementById("upgradeCountA")!;
 const upgradeCountHomemade = document.getElementById("upgradeCountB")!;
 const upgradeCountChef = document.getElementById("upgradeCountC")!;
+const upgradeCountWholeFoods = document.getElementById("upgradeCountA1")!;
+const upgradeCountThreeMichelinStar =
+  document.getElementById("upgradeCountC1")!;
 
 let upgradeCostSafeway = 10;
+let upgradeCostWholeFoods = 50;
 let upgradeCostHomemade = 100;
 let upgradeCostChef = 1000;
+let upgradeCostThreeMichelinStar = 5000;
 
 // Function to update the count display
 function updateCountDisplay() {
@@ -63,10 +68,16 @@ myButton.addEventListener("click", function () {
 // Add a purchasable upgrade item
 const upgradeButtonSafeway = document.createElement("button");
 const upgradeButtonHomemade = document.createElement("button");
+const upgradeButtonWholeFoods = document.createElement("button");
 const upgradeButtonChef = document.createElement("button");
+const upgradeButtonThreeMichelinStar = document.createElement("button");
 
 function updateUpgradeCosts() {
   upgradeButtonSafeway.textContent = `Safeway Sushi🛒 (Cost: ${upgradeCostSafeway.toFixed(
+    2,
+  )})`;
+
+  upgradeButtonWholeFoods.textContent = `WholeFoods Sushi🥕 (Cost: ${upgradeCostWholeFoods.toFixed(
     2,
   )})`;
 
@@ -77,11 +88,17 @@ function updateUpgradeCosts() {
   upgradeButtonChef.textContent = `Sushi Chef🍱 (Cost: ${upgradeCostChef.toFixed(
     2,
   )})`;
+
+  upgradeButtonThreeMichelinStar.textContent = `Three Michelin Star Sushi⭐️ (Cost: ${upgradeCostThreeMichelinStar.toFixed(
+    2,
+  )})`;
 }
 
 upgradeButtonSafeway.disabled = true; // Disable by default
+upgradeButtonWholeFoods.disabled = true;
 upgradeButtonHomemade.disabled = true; // Disable by default
 upgradeButtonChef.disabled = true; // Disable by default
+upgradeButtonThreeMichelinStar.disabled = true;
 
 upgradeButtonSafeway.addEventListener("click", function () {
   if (count >= upgradeCostSafeway) {
@@ -97,13 +114,27 @@ upgradeButtonSafeway.addEventListener("click", function () {
     checkUpgradeAffordability(); // Recheck affordability after purchase
   }
 });
+upgradeButtonWholeFoods.addEventListener("click", function () {
+  if (count >= upgradeCostWholeFoods) {
+    count -= upgradeCostWholeFoods;
+    upgradeCostWholeFoods = upgradeCostWholeFoods * 1.15;
+    growthRate += 1;
+    autoIncrementActive = true;
+    upgradeItems[1].count++;
+    updateUpgradeCosts();
+    updateCountDisplay();
+    updateGrowthRateDisplay();
+    updateUpgradeCounts();
+    checkUpgradeAffordability();
+  }
+});
 upgradeButtonHomemade.addEventListener("click", function () {
   if (count >= upgradeCostHomemade) {
     count -= upgradeCostHomemade; // Deduct 10 units
     upgradeCostHomemade = upgradeCostHomemade * 1.15;
     growthRate += 2.0; // Increment the growth rate
     autoIncrementActive = true; // Enable auto-increment after purchase
-    upgradeItems[1].count++;
+    upgradeItems[2].count++;
     updateUpgradeCosts();
     updateCountDisplay();
     updateUpgradeCounts();
@@ -117,12 +148,26 @@ upgradeButtonChef.addEventListener("click", function () {
     upgradeCostChef = upgradeCostChef * 1.15;
     growthRate += 50; // Increment the growth rate
     autoIncrementActive = true; // Enable auto-increment after purchase
-    upgradeItems[2].count++;
+    upgradeItems[3].count++;
     updateUpgradeCosts();
     updateUpgradeCounts();
     updateCountDisplay();
     updateGrowthRateDisplay();
     checkUpgradeAffordability(); // Recheck affordability after purchase
+  }
+});
+upgradeButtonThreeMichelinStar.addEventListener("click", function () {
+  if (count >= upgradeCostThreeMichelinStar) {
+    count -= upgradeCostThreeMichelinStar;
+    upgradeCostThreeMichelinStar = upgradeCostThreeMichelinStar * 1.15;
+    growthRate += 500;
+    autoIncrementActive = true;
+    upgradeItems[4].count++;
+    updateUpgradeCosts();
+    updateCountDisplay();
+    updateGrowthRateDisplay();
+    updateUpgradeCounts();
+    checkUpgradeAffordability();
   }
 });
 
@@ -134,16 +179,20 @@ app.append(header);
 const upgradeContainer = document.createElement("div");
 upgradeContainer.style.textAlign = "center"; // Center-align the container
 upgradeContainer.appendChild(upgradeButtonSafeway);
+upgradeContainer.appendChild(upgradeButtonWholeFoods);
 upgradeContainer.appendChild(upgradeButtonHomemade);
 upgradeContainer.appendChild(upgradeButtonChef);
+upgradeContainer.appendChild(upgradeButtonThreeMichelinStar);
 app.append(upgradeContainer);
 
 const upgradeCountContainer = document.createElement("div");
 upgradeCountContainer.style.textAlign = "center";
 upgradeCountContainer.appendChild(growthRateDisplay);
 upgradeCountContainer.appendChild(upgradeCountSafeway);
+upgradeCountContainer.appendChild(upgradeCountWholeFoods);
 upgradeCountContainer.appendChild(upgradeCountHomemade);
 upgradeCountContainer.appendChild(upgradeCountChef);
+upgradeCountContainer.appendChild(upgradeCountThreeMichelinStar);
 app.append(upgradeCountContainer);
 
 // Function to check if the upgrade can be afforded
@@ -152,6 +201,11 @@ function checkUpgradeAffordability() {
     upgradeButtonSafeway.disabled = false;
   } else {
     upgradeButtonSafeway.disabled = true;
+  }
+  if (count >= upgradeCostWholeFoods) {
+    upgradeButtonWholeFoods.disabled = false;
+  } else {
+    upgradeButtonWholeFoods.disabled = true;
   }
   if (count >= upgradeCostHomemade) {
     upgradeButtonHomemade.disabled = false;
@@ -162,6 +216,11 @@ function checkUpgradeAffordability() {
     upgradeButtonChef.disabled = false;
   } else {
     upgradeButtonChef.disabled = true;
+  }
+  if (count >= upgradeCostThreeMichelinStar) {
+    upgradeButtonThreeMichelinStar.disabled = false;
+  } else {
+    upgradeButtonThreeMichelinStar.disabled = true;
   }
 }
 
@@ -174,14 +233,18 @@ interface UpgradeItem {
 
 const upgradeItems: UpgradeItem[] = [
   { id: "A", cost: upgradeCostSafeway, growthRate: 0.1, count: 0 },
-  { id: "B", cost: upgradeCostHomemade, growthRate: 2.0, count: 0 },
-  { id: "C", cost: upgradeCostChef, growthRate: 50, count: 0 },
+  { id: "B", cost: upgradeCostWholeFoods, growthRate: 1, count: 0 },
+  { id: "C", cost: upgradeCostHomemade, growthRate: 2.0, count: 0 },
+  { id: "D", cost: upgradeCostChef, growthRate: 50, count: 0 },
+  { id: "E", cost: upgradeCostThreeMichelinStar, growthRate: 500, count: 0 },
 ];
 
 function updateUpgradeCounts() {
-  upgradeCountSafeway.textContent = `Safeway Sushi🛒: ${upgradeItems[0].count}`;
-  upgradeCountHomemade.textContent = `Homemade Sushi🔪: ${upgradeItems[1].count}`;
-  upgradeCountChef.textContent = `Sushi Chef🍱: ${upgradeItems[2].count}`;
+  upgradeCountSafeway.textContent = `Safeway Sushi🛒(Not the best sushi in the world but not too bad): ${upgradeItems[0].count}`;
+  upgradeCountWholeFoods.textContent = `WholeFoods Sushi🥕(Better than safeway for sure but still not the best): ${upgradeItems[1].count}`;
+  upgradeCountHomemade.textContent = `Homemade Sushi🔪(You are quite the home cook, pretty decent sushi): ${upgradeItems[2].count}`;
+  upgradeCountChef.textContent = `Sushi Chef🍱(Great sushi, very delicious): ${upgradeItems[3].count}`;
+  upgradeCountThreeMichelinStar.textContent = `Three Michelin Star Sushi⭐️(The best sushi in the world): ${upgradeItems[4].count}`;
 }
 
 updateUpgradeCounts();
